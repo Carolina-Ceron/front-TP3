5de30d50fd903d241955f715bbbd68ac4fa1fbe2
Carolina-Ceron <carol.ceron801@gmail.com>
2024-11-26 20:17:36 -0300

    TP3 final

--- main.js
+++ main.js
@@ -5,12 +5,21 @@
 -realizar la formula de area
 -imprimir por consola el resultado
 */
-function calcularArea() {
+function ejercicio1() {
     const largo = document.getElementById('largo').value;
     const ancho = document.getElementById('ancho').value;
-    const area = largo*ancho; 
+    const area = calcularArea(largo, ancho); 
+    console.log("Ejercicio 1")
     console.log(area); 
 }
+function calcularArea(base,altura){
+    return `El area del rectangulo es ${base*altura}`;
+}
+
+console.log(`Ejercicio 1: ${calcularArea(3,5)}`);
+console.log(`Ejercicio 1: ${calcularArea(4,5)}`);
+console.log(`Ejercicio 1: ${calcularArea(5,5)}`);
+
 
 /* -------------- 2. Contar palabras en una cadena -------------- 
 -recibir la cadena por input
@@ -18,11 +27,19 @@ function calcularArea() {
 -separar las palabras filtrando los espacios en blanco
 -imprimir por consola el resultado de la variable creada
 */
-function contarPalabras() {
+function ejercicio2() {
     const texto = document.getElementById('texto').value;
-    const palabras = texto.split(" ").filter(palabra => palabra !== "");
-    console.log("El texto tiene", palabras.length, "palabras.");
+    console.log(contarPalabras(texto));
 }
+
+function contarPalabras(texto) {
+    const palabras = texto.split(" ").filter(palabra => palabra !== "");
+    return `Ejercicio 2: El texto ${texto} tiene ${palabras.length} palabras`
+}
+
+console.log(`${contarPalabras("asd asd asd")}`);
+console.log(`${contarPalabras("asd asdv")}`);
+console.log(`${contarPalabras("asd rdvdrb fdsfvs asdv")}`);
 
 /* -------------- 3. Invertir una cadena -------------- 
 -recibir la cadena por input
@@ -33,13 +50,21 @@ function contarPalabras() {
 -crear una variable para unificar los caracteres invertidos
 -mostrar por consola la cadena de caracteres invertidos
 */
-function invertirCadena() {
+function ejercicio3() {
     const cadena = document.getElementById('invertir').value;
+    console.log(invertirCadena(cadena));
+}
+
+function invertirCadena(cadena) {
     const caracteres = cadena.split('');
     const caracteresInvertidos = caracteres.reverse();
     const cadenaInvertida = caracteresInvertidos.join('');
-    console.log(cadenaInvertida);
+    return `Ejercicio 3: El texto ${cadena} invertido es ${cadenaInvertida}`
 }
+
+console.log(invertirCadena("asd dasd"));
+console.log(invertirCadena("asd sad  dasd"));
+console.log(invertirCadena("asd  hgtfdhfgh ht htfghbfg dasd"));
 
 /* -------------- 4. Encontrar el palíndromo -------------- 
 -recibir la cadena por input y pasarla a minuscula
@@ -47,17 +72,32 @@ function invertirCadena() {
 -separar los caracteres, invertirlos y unirlos
 -mostrar por consola el resultado de la comparacion entre la cadena recibida y la cadena invertida
 */
-function esPalindromo() {
-    const cadenaProcesada = document.getElementById('palindromo').value.toLowerCase();
-    const cadenaInvertida = cadenaProcesada.split('').reverse().join('');
-    console.log(cadenaProcesada === cadenaInvertida);
+  function ejercicio4() {
+    const cadenaProcesada = document.getElementById('palindromo').value;
+    console.log(esPalindromo(cadenaProcesada));
   }
 
+function esPalindromo(cadenaProcesada) {
+
+    const cadenaLimpia = cadenaProcesada.toLowerCase().split('').filter(palabra => palabra !== " ");
+    return `Ejercicio 4: El texto ${cadenaProcesada} ${cadenaLimpia.join('') === cadenaLimpia.reverse().join('') ? "es" : "no es"} palindromo`
+}
+
+console.log(esPalindromo("Anita lava la tina"));
+console.log(esPalindromo("Anita no lava la tina"));
+console.log(esPalindromo("Anita lavaba la tina"));
+
+
 /* -------------- 5. Crear un programa para convertir la edad de un perro a años humanos --------------
--recibir el numero por input
+-recibir el numero por prompt
 -mostrar por consola el resultado de el numero recibido multiplicado por 7
 */
+
 function edadHumana() {
-    const edad = document.getElementById('edad').value*7;
-    console.log(`El perro tiene ${edad} años humanos`);
+    const edad = prompt("Ingresa la edad del perro: ");
+    if (edad == Number(edad)){
+        console.log(`Ejercicio 5: El perro tiene ${edad*7} años humanos`);
+    }else {
+        console.log("Ingresa un numero, maestro");
+    }        
 }
